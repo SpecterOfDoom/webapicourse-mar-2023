722fd176089bd449427ab8e2f1f01631f7f13a09
Matt George <mgeorge1@progressive.com>
2023-03-21 14:12:54 -0400

    after lunch

--- EmployeesSolution/EmployeesApi/Domain/DepartmentsLookup.cs
+++ EmployeesSolution/EmployeesApi/Domain/DepartmentsLookup.cs
@@ -1,11 +1,19 @@
-﻿namespace EmployeesApi.Domain;
+﻿using EmployeesApi.Adapaters;
+using Microsoft.IdentityModel.Tokens;
+
+namespace EmployeesApi.Domain;
 
 public class DepartmentsLookup
 {
+    private readonly EmployeesDataContext _context;
+
+    public DepartmentsLookup(EmployeesDataContext context) 
+    {
+        _context = context;
+    }
     public async Task<List<DepartmentItem>> GetDepartmentsAsync()
     {
-
-        return new List<DepartmentItem>
+                return new List<DepartmentItem>
         {
             new DepartmentItem("DEV", "Developers"),
             new DepartmentItem("QA", "Quality Assurance Analysts"),
